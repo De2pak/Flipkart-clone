3892a1b36ebd84e8b965ec93cc4cb0eff5c34561
De2pak <de2pak.bhatt@gmail.com>
2024-04-28 17:18:04 +0530

    Some more content added

---
+++ script.js
@@ -0,0 +1,5 @@
+const rightBtn = document.querySelector("#btn2");
+const leftBtn = document.querySelector("#btn1");
+const slider = document.querySelector(".sliderImg");
+let images = document.querySelectorAll(".images");
+let image = Array.from(images);
